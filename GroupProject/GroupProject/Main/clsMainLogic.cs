7efd7fa792e6809444bdd898d32ce08ea907d238
Alden Park <alden.park@clearlink.com>
2019-04-06 11:07:47 -0600

    Updated Business Logic

--- GroupProject/GroupProject/Main/clsMainLogic.cs
+++ GroupProject/GroupProject/Main/clsMainLogic.cs
@@ -8,6 +8,34 @@ namespace GroupProject
 {
     class clsMainLogic
     {
+        // What business logic is there?
 
+        //public static List<Definitions> GetDefinitions()
+        //{
+
+        //    DataSet ds = new DataSet();
+        //    int iRet = 0;
+        //    List<Definitions> ObjectVar = new List<Definitions>();
+        //    clsDataAccess clsData = new clsDataAccess();
+        //    try
+        //    {
+        //        ds = clsData.ExecuteSQLStatement(clsMainSQL.SomeQuery, ref iRet);
+
+        //        for (int i = 0; i < iRet; i++)
+        //        {
+        //            ObjectVar.Add(new Definitions()
+        //            {
+        //                Id = Int32.Parse(ds.Tables[0].Rows[i][0].ToString()),
+        //                Name = ds.Tables[0].Rows[i][i].ToString().ToString()
+        //            });
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+        //    }
+
+        //    return ObjectVar;
+        //}
     }
 }
--- GroupProject/GroupProject/Main/clsMainSQL.cs
+++ GroupProject/GroupProject/Main/clsMainSQL.cs
@@ -11,59 +11,13 @@ namespace GroupProject.Main
 {
     class clsMainSQL
     {
-
-        // Update to only have SQL code in it
-
-        /// <summary>
-        /// Load List of Definitions for Invoices
-        /// </summary>
-        /// <returns></returns>
-        //public static List<Definitions> GetDefinitions()
+        // What do I need to quiry as part of the business logic?
+        //internal static string someQuery
         //{
-
-        //    DataSet ds = new DataSet();
-        //    int iRet = 0;
-        //    List<Definitions> ObjectVar = new List<Definitions>();
-        //    clsDataAccess clsData = new clsDataAccess();
-        //    try
+        //    get
         //    {
-        //        ds = clsData.ExecuteSQLStatement("SELECT id, defname FROM Definitions", ref iRet);
-
-        //        for (int i = 0; i < iRet; i++)
-        //        {
-        //            ObjectVar.Add(new Definitions()
-        //            {
-        //                Id = Int32.Parse(ds.Tables[0].Rows[i][0].ToString()),
-        //                Name = ds.Tables[0].Rows[i][i].ToString().ToString()
-        //            });
-        //        }
+        //        return "Select field1, field2 from SOMETABLE";
         //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
-        //    }
-
-        //    return ObjectVar;
-        //}
-
-        ///// <summary>
-        ///// Save New Definition
-        ///// </summary>
-        ///// <param name="name"></param>
-        ///// <returns></returns>
-        //public static List<Definitions> SaveDefinitions(String name)
-        //{
-        //    return clsMainSQL.GetDefinitions();
-        //}
-
-        ///// <summary>
-        ///// Delete Definition if it is not longer used
-        ///// </summary>
-        ///// <param name="id"></param>
-        ///// <returns></returns>
-        //public static bool DeleteDefinition(int id)
-        //{
-        //    return false;
         //}
 
     }
